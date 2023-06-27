f1df95f1a1621744ac661e66bd7625993680a219
Manferno <manuel.rcb.mc@gmail.com>
2023-06-27 17:57:41 -0400

    first

---
+++ src/HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        String nombre = "Manuel Roberth Cruces Bascuñán";
+
+        System.out.println("nombre = " + nombre);
+    }
+}
