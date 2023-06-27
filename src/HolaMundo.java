b30ef7e1cd43a4d159153cf4c8b1dc8a552ad636
Manferno <manuel.rcb.mc@gmail.com>
2023-06-27 18:29:00 -0400

    Variables

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,7 +1,34 @@
+import java.util.Locale;
+
 public class HolaMundo {
     public static void main(String[] args) {
         String nombre = "Manuel Roberth Cruces Bascuñán";
 
-        System.out.println("nombre = " + nombre);
+        System.out.println("nombre = " + nombre.toUpperCase(Locale.ROOT));
+
+
+        boolean valor = true;
+        int numero = 10;
+        int numero2= 3;
+        if (valor){
+            System.out.println("Número= " + numero );
+            numero2=10;
+        }
+        System.out.println("Número 2= "+ numero2);
+        var numero3 = "14";
+
+        String name;
+        name = "James";
+        if (numero>2){
+            name = "Peter";
+            System.out.println(name);
+        }
+
+        // int edadPersona = 5; comentario señores
+        /*
+        Comentario más largo
+         */
+
+
     }
 }
