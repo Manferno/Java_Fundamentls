d0718cdd94c6dfec62d9b1683f589383ec7f2e14
Manferno <manuel.rcb.mc@gmail.com>
2023-07-01 23:25:39 -0400

    Aritmetic intro

---
+++ src/OperadorAritmetico.java
@@ -0,0 +1,19 @@
+public class OperadorAritmetico {
+    public static void main(String[] args) {
+
+        int j=4, i=5;
+
+        int suma = j + i;
+        System.out.println("suma = " + suma);
+
+        int resta = j - i;
+        System.out.println("resta = " + resta);
+        System.out.println("(j - i = " + (j - i));
+
+        int multi = j * i;
+        System.out.println("multi = " + multi);
+
+        float div = 5 / 4;
+        System.out.println("div = " + div);
+    }
+}
